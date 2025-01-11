8275363a607cf1fdce457be9465bd45eea10e348
Asadullah3030 <160203993+Asadullah3030@users.noreply.github.com>
2025-01-11 12:27:13 +0000

    comparsion in js

---
+++ script/class_3.js
@@ -0,0 +1,13 @@
+// comparsion in javaScript
+
+console.log('1' > 0)  // true
+console.log('01' > 0) // true
+
+console.log(null > 0) // false
+console.log(null == 0) // false
+console.log(null >= 0) // phelay wo 0 may convert karta ha  // true
+console.log(undefined == 0) // false
+
+// ===
+
+console.log('2' === 2) // false/true
