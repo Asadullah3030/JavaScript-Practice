66b15de5a93c7964460efdb415e93552719db731
Asadullah3030 <160203993+Asadullah3030@users.noreply.github.com>
2025-01-11 07:57:31 +0000

    new file class_2

--- script/class_1.js
+++ script/class_1.js
@@ -1,5 +1,14 @@
+//                                      Variable in js                               //
+
+// Let,Const & Var 
+
 let myNAME = 'ASADULLAH'
 const myId = 12345
 var myEmail = 'asad@gmail.com'
 
 myPassword = '12345678'
+myName = 'Abdullah'
+let myFather;
+
+
+console.table([myNAME,myPassword,myEmail,myId,myFather])
