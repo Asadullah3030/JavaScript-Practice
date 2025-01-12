66b01ab1fc827824dafaad59d0d75a8657c9ba94
Asadullah3030 <160203993+Asadullah3030@users.noreply.github.com>
2025-01-12 09:34:19 +0000

    Date in js

---
+++ script/class_6_Date.js
@@ -0,0 +1,22 @@
+let date = new Date()
+console.log(date);
+
+
+let personalDate = new Date(2024,0,12 ,12,20,45)
+console.log(personalDate.toDateString());
+console.log(personalDate.toTimeString());
+console.log(personalDate.toLocaleString());
+
+
+// const now = new Date();
+// console.log(now.getFullYear()); // Example: 2025
+
+// console.log(now.getMonth()); // Example: 0 (January)
+// console.log(now.getDate()); // Example: 12
+// console.log(now.getDay()); // Example: 6 (Saturday)
+// console.log(now.getHours()); // Example: 14 (2 PM)
+// console.log(now.getMinutes()); // Example: 45
+// console.log(now.getSeconds()); // Example: 30
+// now.setFullYear(2030);
+// console.log(now); // Updated year: 2030
+// console.log(now.toLocaleString()); // Example: "1/12/2025, 2:45:00 PM"
