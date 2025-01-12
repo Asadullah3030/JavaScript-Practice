8d0b54cfb8a1dc69f4057efe7f4f6b887a2a8878
Asadullah3030 <160203993+Asadullah3030@users.noreply.github.com>
2025-01-12 05:41:27 +0000

    strings in js

---
+++ script/class_4_String.js
@@ -0,0 +1,63 @@
+//                                         string method in javaScript 
+
+let firstName = 'asadullah'
+let lastName = 'ashraf'
+
+// let fullName = firstName + lastName
+console.log(`my first name ${firstName} and last name is ${lastName}`)  // using template literal
+
+
+// 1 length
+
+let myName = 'asadullah'
+console.log(myName.length)
+
+// 2 upperCase
+
+let upperC = 'asadullah'
+console.log(upperC.toUpperCase())
+
+// 3 lowerCase
+
+let lowerC = 'asadullah'
+console.log(lowerC.toLocaleLowerCase())
+
+// 4 charAt
+
+let charAt = 'asadullah'
+console.log(charAt.charAt(6))
+
+// 5 indexOf
+
+let indexOf = 'asadullah'
+console.log(indexOf.indexOf('h'))
+
+// 6 split  // convert string into array
+
+let split = 'asadullah'
+console.log(split.split(','))
+
+// 7 replace
+
+let replaceM = 'asadullah is my childHood name'
+console.log(replaceM.replace('childHood','naturely'))
+
+// 8 slice
+
+let sliceM = 'asadullah'
+console.log(sliceM.slice(-4,6))
+
+// 9 substring
+
+let subStringM = 'asadullah'
+console.log(subStringM.substring(0,3))
+
+// 10 trim
+
+let trimM = '   asadullah  '
+console.log(trimM.trim())
+
+// 11 inculideM
+
+let includeseM = 'asadullah'
+console.log(includeseM.includes('h'))
